0d6bc5fb76f0ed3cb55cfc660eb65c25eb194265
RiyaMehla0030 <141139145+RiyaMehla0030@users.noreply.github.com>
2023-10-02 09:38:30 -0700

    counter up final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,85 +1,87 @@
 import React from 'react';
-import { useEffect } from 'react';
-import $ from "jquery";
-import "jquery.counterup";
+import { useEffect, useState } from 'react';
+import CountUp from "react-countup";
 
 
 const Footer = () => {
+    const [count, setCount] = useState(0);
+
     useEffect(() => {
-        $(".count-heading").counterUp({
-          delay: 10,
-          time: 1200,
-        });
-      }, []);
-  return (
-    <div>
-       <footer>
-        <div class="footer-section">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="count1">
-                        <h2 class="count-heading">97</h2>
-                        <span class="count-span">%</span>
-                        <p class="sub-heading mt-2">Graduates</p>
+        const interval = setInterval(() => {
+            setCount(count + 1);
+        }, 1000);
 
-                    </div>
-                    <div class="count2">
-                        <h2 class="count-heading mt-5">30</h2>
-                        <span class="count-span">+</span>
-                        <p class="sub-heading mt-2">Certified Teachers</p>
+        return () => clearInterval(interval);
+    }, [count]);
+    return (
+        <div>
+            <footer>
+                <div class="footer-section">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="count1">
+                                <h2 class="count-heading"> <CountUp end={97} /></h2>
+                                <span class="count-span">%</span>
+                                <p class="sub-heading mt-2">Graduates</p>
 
-                    </div>
-                    <div class="count3">
-                        <h2 class="count-heading mt-5">8</h2>
-                        <p class="sub-heading mt-2">Student campuses</p>
+                            </div>
+                            <div class="count2">
+                                <h2 class="count-heading mt-5"> <CountUp end={30} /></h2>
+                                <span class="count-span">+</span>
+                                <p class="sub-heading mt-2">Certified Teachers</p>
 
-                    </div>
-                    <div class="count4">
-                        <h2 class="count-heading mt-5">650</h2>
-                        <p class="sub-heading mt-2">Students</p>
+                            </div>
+                            <div class="count3">
+                                <h2 class="count-heading mt-5">  <CountUp end={8} /></h2>
+                                <p class="sub-heading mt-2">Student campuses</p>
 
-                    </div>
-                </div>
-              
-                <div class="col-lg-4">
-                    <h3 class="text-white heading-section" >FEEDBACK</h3>
-                    <div class="tweet mt-4"><i class="fa-brands fa-twitter"></i></div>
-                    <div class="tweet mt-4"><i class="fa-brands fa-whatsapp"></i></div>
-                    <div class="tweet mt-4"><i class="fa-brands fa-facebook"></i></div>
+                            </div>
+                            <div class="count4">
+                                <h2 class="count-heading mt-5">  <CountUp end={650} /></h2>
+                                <p class="sub-heading mt-2">Students</p>
 
-                </div>
-                <div class="col-lg-5">
-                    <h4 class="text-white heading-section">ABOUT US</h4>
-                    <p class="sub-heading mt-3">Since 1990, our college provides students with high-quality professional
-                        education. Together with us,
-                        you will have deeper knowledge of the subjects important for your future profession.</p>
-                    <h4 class="text-white heading-section mt-5">NEWSLETTER</h4>
-                    <p class="sub-heading mt-3">Keep up with our weekly news and updates on education. Enter your e-mail
-                        and
-                        subscribe to our
-                        newsletter.
-                    </p>
-                    <div class="mail">
-                        <div class="e-mail">
-                            <input type="text" placeholder="Enter your e-mail"/>
+                            </div>
                         </div>
-                        <div class="mail-btn">
-                            <i class="fa-solid fa-envelope"></i>
+
+                        <div class="col-lg-4">
+                            <h3 class="text-white heading-section" >FEEDBACK</h3>
+                            <div class="tweet mt-4"><i class="fa-brands fa-twitter"></i></div>
+                            <div class="tweet mt-4"><i class="fa-brands fa-whatsapp"></i></div>
+                            <div class="tweet mt-4"><i class="fa-brands fa-facebook"></i></div>
 
                         </div>
+                        <div class="col-lg-5">
+                            <h4 class="text-white heading-section">ABOUT US</h4>
+                            <p class="sub-heading mt-3">Since 1990, our college provides students with high-quality professional
+                                education. Together with us,
+                                you will have deeper knowledge of the subjects important for your future profession.</p>
+                            <h4 class="text-white heading-section mt-5">NEWSLETTER</h4>
+                            <p class="sub-heading mt-3">Keep up with our weekly news and updates on education. Enter your e-mail
+                                and
+                                subscribe to our
+                                newsletter.
+                            </p>
+                            <div class="mail">
+                                <div class="e-mail">
+                                    <input type="text" placeholder="Enter your e-mail" />
+                                </div>
+                                <div class="mail-btn">
+                                    <i class="fa-solid fa-envelope"></i>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="copyright mt-5">
+                            <div class="copyright-left text-muted">©2023 All Rights Reserved. Terms of Use and Privacy Policy.
+                            </div>
+                            <div class="copyright-right text-muted">Designed by Riya.
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="copyright mt-5">
-                    <div class="copyright-left text-muted">©2023 All Rights Reserved. Terms of Use and Privacy Policy.
-                    </div>
-                    <div class="copyright-right text-muted">Designed by Riya. 
-                    </div>
-                </div>
-            </div>
+            </footer>
         </div>
-    </footer>
-    </div>
-  )
+    )
 }
 
 export default Footer
