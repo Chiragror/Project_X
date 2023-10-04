c9781860e13a2031e8f2a04e5bc107b5bf93e0cd
Chirag Singh <112153220+Chiragror@users.noreply.github.com>
2023-10-04 22:10:42 +0530

    logo add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="logo">CS</div>
+      <div className="logo" >
+        <img className="mi" src="\main icon.png" alt="logo" />
+        </div> 
       <ul className="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
