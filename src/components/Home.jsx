fd28f10d50828a37dfe7a830f4782ad4017643e8
Chirag Singh <112153220+Chiragror@users.noreply.github.com>
2023-10-04 22:46:44 +0530

    b

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='main'>
       
       <h1>Welcome to Our Website</h1>
       <p>This is the home page content.</p>
